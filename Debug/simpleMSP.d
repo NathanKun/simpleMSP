b2c17d0f34fb41300036511f51713e002524b8c5
JunyangHE <junyang.he@groupe-esigelec.org>
2016-12-09 18:41:29 +0100

    添加已有的代码，并开始在github上开发
    已有函数：
    directionPin
    digitalWrite
    digitalRead
    selectPin
    pullupPin
    interruptPin
    enableS2
    initPorts
    analogRead
    timer_init
    timer_create
    timer_delete

---
+++ Debug/simpleMSP.d
@@ -0,0 +1,21 @@
+# FIXED
+
+simpleMSP.obj: ../simpleMSP.c
+simpleMSP.obj: D:/ti/ccsv6/ccs_base/msp430/include/MSP430.h
+simpleMSP.obj: D:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+simpleMSP.obj: D:/ti/ccsv6/ccs_base/msp430/include/in430.h
+simpleMSP.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics.h
+simpleMSP.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics_legacy_undefs.h
+simpleMSP.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdint.h
+simpleMSP.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdarg.h
+simpleMSP.obj: ../simpleMSP.h
+
+../simpleMSP.c: 
+D:/ti/ccsv6/ccs_base/msp430/include/MSP430.h: 
+D:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+D:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics_legacy_undefs.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdint.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdarg.h: 
+../simpleMSP.h: 
