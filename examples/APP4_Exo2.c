d767ef146f6cca139ba9c5248561af98c10f282d
JunyangHE <junyang.he@groupe-esigelec.org>
2016-12-15 21:52:47 +0100

    恢复

---
+++ examples/APP4_Exo2.c
@@ -0,0 +1,116 @@
+﻿/**
+ * Exercice 2 : On souhaite réaliser un chronomètre digital affichant les 1/10 de seconde. 
+ * Nous supposons le DCO (SMCLK) réglé à 1MHz.
+ * a) Déterminer la meilleure configuration du prédiviseur, 
+ * mode de comptage et TACCR0 afin d’obtenir une période de timer de 100ms.
+ * b) Ecrire en C une fonction de configuration du timer.
+ * c) Ecrire l’application afin de réaliser l’application chronomètre en y intégrant une fonction void 
+ * afficheTime(char min, char sec, char sec10) permettant d’afficher le chronomètre sur l’afficheur. 
+ * Comme il n’y a que 4 digits sur l’afficheur, nous nous limiterons à l’affichage suivant : M.SS.D
+ * 
+ * Remarque : un chronomètre dispose d’un bouton de déclenchement et d’arrêt. 
+ * Il y a souvent un bouton de remise à zéro. Comme sur la Launchpad, 
+ * il n’y a que le bouton S2 d’accessible, le premier appui déclenchera la mise à zéro 
+ * et le démarrage simultané du chronomètre et le second appui figera le chronomètre sur la valeur mesurée.
+ *
+ * HE Junyang
+ *
+ * 做一个计时器，显示为；分.秒秒.十分之一秒，按钮S2按一下停止，再按清除并重新开始计时。
+ */
+
+#include <msp430.h> 
+#include "Afficheur.h"
+#include "simpleMSP.h" // 引入库
+
+volatile char min, sec, sec10; // 用于显示的三个时间变量
+volatile uint8_t is_stop = 1; // 是否停止，1停，0走
+
+/**
+ * 显示时间
+ */
+void afficheTime (char min, char sec, char sec10) {
+	Aff_valeur(convert_Hex_Dec(min*1000 + sec*10 + sec10)); // 分钟第四位，乘1000，秒第二位，乘10
+}
+
+/*
+ * main.c
+ */
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+
+
+    initPorts(); // 初始化全部针
+	
+    // configuration des I/O
+    /*P1DIR &= ~BIT3;
+    P1OUT |= BIT3;
+    P1REN |= BIT3;
+    P1IE |= BIT3;
+    P1IES |= BIT3;*/
+
+    enableS2(); // 启用按钮S2
+    interruptPin(P13, ENABLE, FALLING_EDGE); // 启用P13（按钮S2）的中断，检测下降沿
+
+    // configuration de timer
+    /*BCSCTL1 = CALBC1_1MHZ;
+    DCOCTL = CALDCO_1MHZ;*/
+    timer_init(); // 初始化计时器
+    /*
+    TA0CTL = MC_1 | TASSEL_2 | ID_1; // up mode, SMCLK, diviser par 2
+    TA0CCR0 = 50000; //100ms
+    TA0CTL |= TAIE; // activer interrution de timer
+     */
+
+    Aff_Init(); // 初始化显示器
+
+
+    __enable_interrupt(); // 全局开启中断
+
+    directionPin(P10, OUTPUT); // 将P10设为输出模式
+    is_stop = 1;
+	while(1){ // 停止时亮灯，计时时关灯
+		if(is_stop == 1) 
+			digitalWrite(P10, HIGH); // 将P10输出设为高电平
+		else
+			digitalWrite(P10, LOW); // 将P10输出设为低电平
+	}
+	
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void) { // 按钮S2中断时执行的函数
+	if(is_stop == 0) {
+		is_stop = 1;
+		timer_delete(TIMER0); // 删除计时器
+	} else if(is_stop == 1){
+		is_stop = 0;
+		sec10 = sec = min = 0; // 重置变量
+		Aff_Efface();
+	    Aff_Points(BIT0 | BIT2); // afficher points
+	    timer_create(TIMER0, 100000, UP_MODE); // 创建计时器，ccr为100000微秒，Up模式，即100毫秒触发一次
+	}
+	clearFlag(P13); // 降下P13的触发旗帜
+}
+
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void my_timer() { // 计时器中断函数
+	if(is_stop == 0){
+		sec10++; // +0.1s
+		if(sec10 == 10) {
+			sec10 = 0;
+			sec++;
+			Aff_Efface(); // avoid some possible display errors.
+			Aff_Points(BIT0 | BIT2); // afficher points
+		}
+		if(sec == 60){
+			sec = 0;
+			min++;
+		}
+		if(min == 10){
+			min = 0;
+		}
+		afficheTime (min, sec, sec10); // 显示时间
+	}
+
+	clearFlag(TIMER0); // 降下TIMER0的触发旗帜
+}
---
+++ simpleMSP.h
@@ -0,0 +1,252 @@
+/*
+ * simpleMSP.h
+ *
+ *  Created on: 6 dec. 2016
+ *      Author: Junyang HE
+ */
+#ifndef SIMPLEMSP_H_   /* Include guard */
+#include <stdint.h>
+#define SIMPLEMSP_H_
+
+#define HIGH 1
+#define LOW 0
+
+#define OUTPUT 1
+#define INPUT 0
+
+#define ENABLE 1
+#define DISABLE 0
+
+#define RISING_EDGE 1
+#define FALLING_EDGE 0
+
+#define P10 10
+#define P11 11
+#define P12 12
+#define P13 13
+#define P14 14
+#define P15 15
+#define P16 16
+#define P17 17
+
+#define P20 20
+#define P21 21
+#define P22 22
+#define P23 23
+#define P24 24
+#define P25 25
+#define P26 26
+#define P27 27
+
+//typedef enum { P10, P11, P12, P13, P14, P15, P16, P17 } port1Pins;
+//typedef enum { P20, P21, P22, P23, P24, P25, P26, P27 } port2Pins;
+
+typedef enum {
+false = 0, true = 1}bool;
+
+/*
+ * 0000 A0
+ * 0001 A1
+ * 0010 A2
+ * 0011 A3
+ * 0100 A4
+ * 0101 A5
+ * 0110 A6
+ * 0111 A7
+ * 1000 VeREF+
+ * 1001 VREF--/VeREF--
+ * 1010 Temperature sensor
+ * 1011 (VCC – VSS) / 2
+ * */
+typedef enum {A0 = 0,
+A1 = 1,
+A2 = 2,
+A3 = 3,
+A4 = 4,
+A5 = 5,
+A6 = 6,
+A7 = 7,
+VeREFp = 8,
+VREFnByVeREFn = 9,
+Temperature_sensor = 10,
+HalfOfVCCMinusVSS = 11
+} inch;
+
+/**
+ * timer emum
+ */
+typedef enum {
+TIMER0 = 0, TIMER1 = 1
+} timer;
+
+/*
+ * Mode control for timerA
+ * 00 Stop mode: the timer is halted.
+ * 01 Up mode: the timer counts up to TACCR0.
+ * 10 Continuous mode: the timer counts up to 0FFFFh.
+ * 11 Up/down mode: the timer counts up to TACCR0 then down to 0000h.
+ */
+typedef enum {
+STOP_MODE = MC_0, UP_MODE = MC_1, CONTINUOUS_MODE = MC_2, UP_DOWN_MODE = MC_3
+} timer_mode_control;
+
+/**
+ * Output mode for timerA
+ *
+ * OUTMODx bits 7-5
+ *
+ * Output modes 2, 3, 6 and 7 are not useful for TACCR0 beacause EQUx=EQU0
+ *
+ * 000 OUT bit value
+ * 001 Set
+ * 010 Toggle/reset
+ * 011 Set/reset
+ * 100 Toggle
+ * 101 Reset
+ * 110 Toggle/set
+ * 111 Reset/set
+ */
+typedef enum {
+SET = OUTMOD_1,
+TOGGLE_RESET = OUTMOD_2,
+SET_RESET = OUTMOD_3,
+TOGGLE = OUTMOD_4,
+RESET = OUTMOD_5,
+TOGGLE_SET = OUTMOD_6,
+RESET_SET = OUTMOD_7
+} timer_output_mode;
+
+extern const uint8_t port1Pins[];
+extern const uint8_t port2Pins[];
+
+/**
+ * set direction of a pin
+ *
+ * \param pin the pin's name
+ *
+ * \param dir INPUT or OUTPUT
+ */
+void directionPin(const uint8_t pin, const uint8_t dir);
+
+/**
+ * write a digital signal to a pin
+ *
+ * \param pin the pin's name
+ *
+ * \value HIGH or LOW
+ */
+void digitalWrite(const uint8_t pin, const uint8_t value);
+
+/**
+ * read a digital signal from a pin
+ *
+ * \param pin the pin's name
+ *
+ * \return HIGH or LOW
+ */
+int digitalRead(const uint8_t pin);
+
+/**
+ * select function mode for a pin
+ *
+ * \param pin the pin's name
+ *
+ * \param mode 0 to 3
+ */
+void selectPin(const uint8_t pin, const uint8_t mode);
+
+/**
+ * set pull-up for a pin
+ *
+ * \param pin the pin's name
+ *
+ * \param enable ENABLE or DISABLE
+ */
+void pullupPin(const uint8_t pin, const uint8_t enable);
+
+/**
+ * set interrupt for a pin
+ *
+ * \param pin the pin's name
+ *
+ * \param enable ENABLE or DISABLE
+ *
+ * \param edge RISING_EDGE or FALLING_EDGE, FALLING_EDGE by default
+ */
+void interruptPin(const uint8_t pin, const uint8_t enable, ...);
+
+/**
+ * enable button S2
+ */
+void enableS2();
+
+/**
+ * Initialize all ports to output mode and turn off output.
+ */
+void initPorts();
+
+/**
+ * initialization for analog read
+ */
+//void analogReadInit();
+/**
+ * read analog signal
+ *
+ * parameters:
+ * reference Vcc GND, 4 x ADC10CLKs
+ * reference-generator on, voltage 2,5 Volts
+ * ADC10 clock divider = 1, clock source select MCLK, 1MHz
+ * Sample-and-hold source select = ADC10SC bit
+ * Conversion sequence mode = Single-channel-single-conversion
+ *
+ * \param input_channel input channel to read
+ * 		A0, A1, A2, A3, A4, A5, A6, A7,
+ * 		VeREFp, VREFnByVeREFn, Temperature_sensor, HalfOfVCCMinusVSS
+ */
+uint16_t analogRead(const inch input_channel);
+
+/**
+ * write pwm
+ */
+void analogWrite(timer timer, uint16_t ccr0_microsecond, uint16_t ccr1_microsecond);
+
+/**
+ * initialization timer, set 1MHZ
+ */
+void timer_init();
+
+/**
+ * create a SMCLK timer and start counting
+ *
+ * \param timer TIMER0 or TIMER1
+ *
+ * \param microsecond ccr in microsecond,from 0 to 524280
+ *
+ * \param mode_control STOP_MODE, UP_MODE, CONTINUOUS_MODE, UP_DOWN_MODE
+ */
+void timer_create(timer timer, uint32_t microsecond,
+	timer_mode_control mode_control, uint8_t interrupt_enable);
+
+/**
+ * stop and delete a timer
+ *
+ * \param timer TIMER0 or TIMER1
+ */
+void timer_delete(timer timer);
+
+/**
+ * reverse the output of a pin, 1 to 0 or 0 to 1
+ *
+ * \param pin the pin's name
+ */
+void reverseOutput(const uint8_t pin);
+
+/**
+ * clear flag of interruption for a pin or a timer
+ *
+ * \param pin the pin's name, or TIMER0, or TIMER1
+ */
+void clearFlag(const uint8_t pin);
+
+#endif // SIMPLEMSP_H_
+
